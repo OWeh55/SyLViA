14d471f50c3af496040ae56f039745565518c65f
Wolfgang Ortmann <noo@uni-jena.de>
2019-02-05 12:03:59 +0100

    marker polygons

---
+++ v2polygon.h
@@ -0,0 +1,11 @@
+#ifndef _V23D_POLYGON_H
+#define _V23D_POLYGON_H
+
+constexpr int xySize = 10;
+constexpr unsigned char xyA[] = {0, 0, 1, 0, 5, 8, 9, 0, 10, 0, 6, 10, 4, 10, 0, 0};
+constexpr unsigned char xyB[] = {0, 0, 1, 0, 5, 4, 9, 0, 10, 0, 10, 1, 6, 5, 10, 9, 10, 10,
+                                 9, 10, 5, 6, 1, 10, 0, 10, 0, 9, 4, 5, 0, 1, 0, 0
+                                };
+
+
+#endif
